2994a1ee6a00b1dcc703685cdc006158ab6199bb
Marlody <marlodycairo@hotmail.com>
2021-08-18 16:12:48 -0500

    Add entity

---
+++ todoazure.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoazure.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
