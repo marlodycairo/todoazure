3c7f8e357712d93ed0285e6ec6da9688990576c1
Marlody <marlodycairo@hotmail.com>
2021-08-19 01:15:54 -0500

    Unit Test Preparation

---
+++ todoazure.tests/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoazure.tests.Helpers
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
